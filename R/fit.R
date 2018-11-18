bb4608e1ac536e984c1fb93aa4dda7118ac66487
Dampeel <dampeel2000@yahoo.fr>
2018-11-18 15:06:08 +0100

    Add R

---
+++ R/fit.R
@@ -0,0 +1,10 @@
+#' Fonction fit
+#'
+#' @param par (par defaut, "toi")
+#' @keywords fit, pls, plsda
+#' @export
+#' @examples
+#' fit("test")
+fit <- function(par="PLSDA") {
+  print(par)
+}
---
+++ R/predict.R
@@ -0,0 +1,9 @@
+#' Fonction predict
+#'
+#' @keywords fit, pls, plsda
+#' @export
+#' @examples
+#' predict()
+predict <- function() {
+  print("Predict")
+}
